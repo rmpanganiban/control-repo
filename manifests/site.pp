941574a2e2eaa2759207fac9d32d7808b2e6f9ea
reypanganiban <reynaldo.panganiban.jr@gmail.com>
2023-11-07 15:20:04 +0800

    creating the secret_password.txt

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,8 @@
 node puppet.localdomain {
   include role::master
+  file {'secret_password':
+    path    => /etc/secret_password.txt
+    ensure  => file,
+    content => lookup('secret_password'),
+  }
 }
